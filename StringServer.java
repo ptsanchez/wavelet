6f59c4d6d8bf41473a037be1b07fa640061ca775
Patric Sanchez <patrics27@outlook.com>
2023-04-24 08:01:42 -0700

    String Server Lab Report 2

--- StringServer.java
+++ StringServer.java
@@ -4,29 +4,31 @@ import java.net.URI;
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
+    String runningString = "";
+    String str2 = "another test on another line";
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("String: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } else {
+            return runningString;
+        } else if (url.getPath().equals("/add-message")) {
             System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
+            if (url.getPath().contains("/add-message")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters[0].equals("s")) {
+                    String message = parameters[1];
+                    runningString = runningString + message + "\n";
+                    return runningString;
                 }
+            
             }
             return "404 Not Found!";
+        } else {
+            return "Please use '/add-message?s=' to add a string on another line on the home page";
         }
     }
 }
 
-class SearchEngine {
+class StringServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
